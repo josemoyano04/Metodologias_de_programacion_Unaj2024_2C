d2b0fe5f752937f179edc8ff0bc49fd61e508c8d
Jose <josemoyano059@gmail.com>
2024-11-11 11:22:35 -0300

    Adaptacion para .NET FrameWork 4.0

---
+++ C#/Practica 06/Practica06/Clases/Juegos de carta/JodeteJuego.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practica06.Clases.Juegos_de_carta
+{
+    public class JodeteJuego : JuegoDeCartasTemplate
+    {
+        private List<Carta> mazo = MazosCartas.X50CartasEspañolas();
+        private const int CARTAS_INICIALES_POR_JUGADOR = 7;
+
+        public override void repartirCartasIniciales()
+        {
+            List<Carta> cartasDeJugador = new List<Carta>();
+            foreach (Persona jugador in jugadores)
+            {
+                for (int i = 0; i < CARTAS_INICIALES_POR_JUGADOR; i++)
+                    cartasDeJugador.Add(cartasMezcladas.Pop());
+                cartasDeJuegadores.Add(jugador, cartasDeJugador);
+            }
+        }
+
+        public override Persona checkGanador()
+        {
+            foreach (Persona jugador in jugadores)
+            {
+                if (cartasDeJuegadores[jugador].Count == 0) return jugador;
+            }
+            return null;
+        }
+
+        public override void jugarMano()
+        {
+            cartasDescartadas.Push(cartasMezcladas.Pop());
+
+            foreach(Persona jugador in jugadores)
+            {
+                if (cartasDeJuegadores[jugador].Count == 0)
+                    return;
+
+                
+            }
+        }
+    }
+}
--- C#/Practica 06/Practica06/Clases/Templates/JuegoDeCartastemplate.cs
+++ C#/Practica 06/Practica06/Clases/Templates/JuegoDeCartastemplate.cs
@@ -5,44 +5,86 @@ namespace Practica06
 {
 	public abstract class JuegoDeCartasTemplate
 	{
-		List<Persona> jugadores = new List<Persona>();
-		Persona ganador = null;
-		
-		protected void setJugadores(params Persona[] jugador){
+		protected List<Persona> jugadores = new List<Persona>();
+		protected Dictionary<Persona, List<Carta>> cartasDeJuegadores = new Dictionary<Persona, List<Carta>>();
+		protected Dictionary<Persona, int> puntosDeJuegadores = new Dictionary<Persona, int>();
+
+		protected List<Carta> mazo;
+		protected Stack<Carta> cartasMezcladas;
+		protected Stack<Carta> cartasDescartadas;
+
+		protected Persona ganador = null;
+
+        protected void comenzarJuego(params Persona[] jugador)
+        {
+            setJugadores(jugador);
+            mezclarMazo();
+            repartirCartasIniciales();
+
+            while (ganador == null)
+            {
+                jugarMano();
+                ganador = checkGanador();
+            }
+
+			//Una vez encontrado un ganador, se limpian todas las listas y pilas de cartas
+			cartasMezcladas.Clear();
+			cartasDescartadas.Clear();
+			foreach(Persona j in jugadores)
+			{
+				cartasDeJuegadores[j].Clear();
+			}
+        }
+        protected void setJugadores(params Persona[] jugador){
 			foreach (Persona j in jugador) {
 				jugadores.Add(j);
 			}
 		}
+
 		
-		protected void comenzarJuego(params Persona[] jugador){
-			setJugadores(jugador); 
-			mezclarMazo();
-			repartirCartasIniciales();
-			
-			while (ganador == null) {
-				jugarMano();
-				ganador = checkGanador();
+		protected void tomarCartas(Persona jugador){
+			cartasDeJuegadores[jugador].Add(cartasMezcladas.Pop());
+		}
+		protected Carta descartarCartas(Persona jugador){
+			Random random = new Random();
+			int posCartaDescartada = random.Next(0, cartasDeJuegadores[jugador].Count -1);
+			Carta cartaDescartada = cartasDeJuegadores[jugador][posCartaDescartada];
+			cartasMezcladas.Push(cartaDescartada);
+			cartasDeJuegadores[jugador].Remove(cartaDescartada);
+
+			return cartaDescartada;
+		}
+		
+		
+		protected void mezclarMazo()
+		{
+			Random random = new Random();
+			int contCartas = 0;
+			int posCambio;
+
+			//Barajado
+			foreach(Carta carta in mazo)
+			{
+				posCambio = random.Next(contCartas, mazo.Count -1);
+
+				//Intercambio de cartas
+				Carta temp = mazo[contCartas];
+				mazo[contCartas] = mazo[posCambio];
+				mazo[posCambio] = temp;
+				contCartas++;
 			}
-			
-			
-		}
-		
-		protected void jugarMano(){
-			foreach (Persona j in jugadores) {
-				Console.WriteLine("Jugador:{0} → {1}", j.getNombre(), tomarCartas());
-				Console.WriteLine("Jugador:{0} → {1} ", j.getNombre(), descartarCartas());
+
+			//Agregado de cada carta a una pila de cartas
+			if(cartasMezcladas != null) cartasMezcladas.Clear();
+			foreach(Carta carta in mazo)
+			{
+				cartasMezcladas.Push(carta);
 			}
 		}
-		protected string tomarCartas(){
-			return "tomando carta";
-		}
-		protected string descartarCartas(){
-			return "descartando carta";
-		}
 		
-		
-		public abstract void mezclarMazo();
+		//Metodos abstractos:
 		public abstract void repartirCartasIniciales();
+		public abstract void jugarMano();
 		public abstract Persona checkGanador();
 		
 		
